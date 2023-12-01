43564c94f419979b656ca5635f97891848aff4a5
alejndrotorres <alejandrotorres042206@gmail.com>
2023-12-01 12:59:12 -0800

    initial commit

---
+++ Sources.py
@@ -0,0 +1,6 @@
+# Google Snake like game
+# Make a game that is similar to snake game
+# Goals:
+# Be able to move freely in the area provided at one block at a time 
+# When eating the food that pops up, the snake body will grow
+# Be able to choose from different game modes that completely change the game
