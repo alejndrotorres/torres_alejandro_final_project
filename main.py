82b2dac36bb66016b448174881fdecd931e3624e
alejndrotorres <alejandrotorres042206@gmail.com>
2023-12-07 10:53:34 -0800

    Creating initial window for game

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 # This file was created by Alejandro Torres
 
 #import libraries
-import pygame as pg
-from pygame.sprite import Sprite
-import random
-from random import randint
-import os
-from settings import *
-from sprites import *
-from math import floor
+import pygame
+import sys
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self,x = 5
+        self.y = 4
+        self.pos = Vector2(self.x,self.y) #storing 2d data
+    
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x,self.pos.y,cell_size,cell_size)
+        pygame.draw.rect(surface,color,rectangle)
+
+pygame.init() #module in pygame
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
+clock = pygame.time.Clock() 
+
+while True:
+    for event in pygame.event.get(): #event loop is a user input 
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit() #ends code that is ran off (closes code)
+
+    screen.fill((175,215,70)) #RGB tuple
+    pygame.display.update() #draw all elements of the game
+    clock.tick(60) #how many frames per second the while loop can run for
+
 
